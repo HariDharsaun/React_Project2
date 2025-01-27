d5a48d2db50f45961854438befb83c28fbd70798
HariDharsaun <haridharsaun18personal@gmail.com>
2025-01-27 09:38:14 +0530

    deploy issue resolved

--- src/SourceFile.jsx
+++ src/SourceFile.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
-import "./Styling/Style.css";
+import "./Styling.css";
 import "./style.css";
 
 const lyrics = [
